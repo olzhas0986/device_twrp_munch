0351c2a90a0e93005605a9ad8475f16e26b18bc2
olzhas0986 <kdiroljas@gmail.com>
2026-03-18 15:37:07 +0500

    munch: Rebase

--- twrp_munch.mk
+++ twrp_munch.mk
@@ -9,14 +9,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from munch device
 $(call inherit-product, device/xiaomi/munch/device.mk)
 
 PRODUCT_DEVICE := munch
-PRODUCT_NAME := omni_munch
+PRODUCT_NAME := twrp_munch
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO F4
 PRODUCT_MANUFACTURER := xiaomi
@@ -24,6 +24,6 @@ PRODUCT_MANUFACTURER := xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="fox_munch-eng 99.87.36 AP2A.240905.003 eng.runner.20260316.111612 test-keys"
+    PRIVATE_BUILD_DESC="twrp_munch-eng 99.87.36 SP2A.220405.004 eng.admin.20250819.044943 test-keys"
 
-BUILD_FINGERPRINT := None
+BUILD_FINGERPRINT := POCO/twrp_munch/munch:99.87.36/SP2A.220405.004/admin08191532:eng/test-keys
